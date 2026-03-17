d4be003838afacbbcf8c4cf6fb3f097ca38d6bc0
danielltgm <117490416+danielltgm@users.noreply.github.com>
2026-03-17 08:37:17 +0700

    Update - Pertemuan2_GTIA2

--- Pertemuan 2/project2 - Rotasi Objek/mainRotasiObjek.cpp
+++ Pertemuan 2/project2 - Rotasi Objek/mainRotasiObjek.cpp
@@ -28,7 +28,7 @@ int main(int argc, char* argv[])
     glutInitWindowSize(640, 480);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 
-    glutCreateWindow("Segitiga");
+    glutCreateWindow("Daniel Lamganda Tua Gultom");
     glutDisplayFunc(Segitiga);
 
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
